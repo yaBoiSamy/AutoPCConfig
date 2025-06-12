0700b3d9e0de2dd53fdddd9f161009208b3efa27
Samuel Bélanger <samprogrammation@gmail.com>
2025-06-12 01:11:05 -0400

    quick bugfix

--- client.ps1
+++ client.ps1
@@ -1,1 +1,4 @@
-Invoke-WebRequest -Uri "http://192.168.1.136:8000/in.zip" -OutFile "./out.zip"
+$desktopPath = [Environment]::GetFolderPath('Desktop') 
+echo $desktopPath
+Invoke-WebRequest -Uri "http://192.168.1.136:8000/in.zip" -OutFile "$desktopPath/out.zip"
+Pause
